90ae4db269cd0626b0ecb869f6bf6ed1cd003cc3
Logan Born <lcborn4@gmail.com>
2020-06-20 21:26:14 -0400

    timer stopwatch and pause

--- src/app/game/timer/timer.component.ts
+++ src/app/game/timer/timer.component.ts
@@ -7,9 +7,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TimerComponent implements OnInit {
 
-  constructor() { }
+  elapsedTime: any;
+  stopwatch: any;
+  constructor() {
+
+    this.elapsedTime = 0;
+
+  }
 
   ngOnInit() {
+    console.log('starting at 0')
+    this.elapsedTime = 0;
+
+    this.elapsedTime = localStorage.startTime ? JSON.parse(localStorage.startTime) : 0;
+    localStorage.setItem('startTime', JSON.stringify(this.elapsedTime));
+
+    this.runTimer();
+  }
+
+  runTimer() {
+    this.stopwatch = setInterval(() => { this.intervalFunc() }, 1000)
+
+  }
+
+  intervalFunc() {
+    this.elapsedTime += 1;
+    localStorage.setItem('startTime', JSON.stringify(this.elapsedTime));
+  }
+
+  pauseTimeLine() {
+    console.log('pausing stopwatch')
+    clearInterval(this.stopwatch);
   }
 
 }
