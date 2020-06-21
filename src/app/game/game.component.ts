3cd8d751e6b12c6cc75f834adcc79e4f5b019e26
Logan Born <lcborn4@gmail.com>
2020-06-20 21:23:55 -0400

    game component access child

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -1,15 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, AfterViewInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+import {TimerComponent} from './timer/timer.component';
 
 @Component({
   selector: 'app-game',
   templateUrl: './game.component.html',
   styleUrls: ['./game.component.scss']
 })
-export class GameComponent implements OnInit {
+export class GameComponent implements OnInit, AfterViewInit {
 
-  constructor() { }
+  @ViewChild(TimerComponent,{static: false})
+
+  private timerComponent: TimerComponent;
+
+  constructor(private router: Router) { }
 
   ngOnInit() {
+  }
+
+  ngAfterViewInit() {
+    this.timerComponent.pauseTimeLine();
+  }
+
+  endGame() {
+    console.log('endGame')
+
+
+    this.timerComponent.pauseTimeLine();
+    this.router.navigate(['/home']);
+
   }
 
 }
