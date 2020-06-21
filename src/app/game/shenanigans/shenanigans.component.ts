220a1d9afaceca5fff328620295029f8edde666b
Logan Born <lcborn4@gmail.com>
2020-06-20 21:25:58 -0400

    check for shenanigans timer

--- src/app/game/shenanigans/shenanigans.component.ts
+++ src/app/game/shenanigans/shenanigans.component.ts
@@ -7,9 +7,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ShenanigansComponent implements OnInit {
 
+  //1 hour
+  timeLeft: number = 3600; //1 hour
+  interval;
+
   constructor() { }
 
   ngOnInit() {
+
+    console.log('shenanigans interval',this.timeLeft)
+
+    this.shenanigansTimer()
+
+  }
+
+  shenanigansTimer()
+  {
+    this.interval = setInterval(() => {
+      if(this.timeLeft > 0) {
+        this.timeLeft--;
+      } else {
+        this.timeLeft = 3600;
+      }
+    },1000)
+
   }
 
 }
