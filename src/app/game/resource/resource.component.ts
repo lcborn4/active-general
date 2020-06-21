f8cdb5d2bbc173b82def4fb042d2279fd241157e
Logan Born <lcborn4@gmail.com>
2020-06-20 21:25:51 -0400

    check resources timer

--- src/app/game/resource/resource.component.ts
+++ src/app/game/resource/resource.component.ts
@@ -7,9 +7,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ResourceComponent implements OnInit {
 
+  timeLeft: number = 600; //10 minutes
+  interval;
+
   constructor() { }
 
   ngOnInit() {
+
+    console.log('check resources interval',this.timeLeft)
+
+    this.checkResourcesTimer()
+
+  }
+
+  checkResourcesTimer()
+  {
+    this.interval = setInterval(() => {
+      if(this.timeLeft > 0) {
+        this.timeLeft--;
+      } else {
+        this.timeLeft = 600;
+      }
+    },1000)
+
   }
 
 }
